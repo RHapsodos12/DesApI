e9ea17be95938b4d3a5f9c8c56f217a3a0430b11
Santacruz <Santacruz@DESKTOP-8F7UTTO.lan>
2018-05-24 20:57:03 -0500

    c

---
+++ DesApI/src/Parcial1/HelloWorld.java
@@ -0,0 +1,35 @@
+package Parcial1;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HelloWorld extends JFrame
+{
+	private JLabel lbl_hello, lbl_hello1, lbl_hello2;
+	
+	public HelloWorld()
+	{
+		super("Hello World");
+		setLayout(new FlowLayout());
+		setSize(200, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		lbl_hello=new JLabel("Hello World!");
+		lbl_hello1=new JLabel ("3 A TIC's");
+		lbl_hello2=new JLabel("Vamos por la acreditación!!!");
+			
+		add(lbl_hello);
+		add(lbl_hello1);
+		add(lbl_hello2);
+		//pack();
+		
+		setVisible(true);
+	}
+
+	public static void main(String[] args) 
+	{
+		HelloWorld ventana= new HelloWorld();
+	}
+}
---
+++ DesApI/src/Parcial1/HelloWorldDos.java
@@ -0,0 +1,40 @@
+package Parcial1;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HelloWorldDos extends JFrame 
+{
+	private JLabel lbl_mensaje;
+	private JButton btn_aceptar;
+	
+	public HelloWorldDos()
+	{
+		super("Hello World 2");
+		setLayout(null);
+		setSize(300, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		lbl_mensaje=new JLabel("Aqui está la etiqueta");
+		lbl_mensaje.setBounds(50, 10, 150, 30);
+		
+		add(lbl_mensaje);
+		
+		btn_aceptar=new JButton("Aceptar");
+		btn_aceptar.setBounds(70, 50, 100, 30);
+		
+		add(btn_aceptar);
+		
+		//pack();
+		
+		setVisible(true);
+	}
+
+	public static void main(String[] args) 
+	{
+		HelloWorldDos ventana=new HelloWorldDos();
+	}
+}
